54bc4f84e2a1111113faf5b3a25d141325ef82f4
Aryan <a94h.73@gmail.com>
2021-03-29 19:45:04 +0430

    Added Activities

---
+++ app/src/main/java/com/example/legendofbounca/GyroActivity.java
@@ -0,0 +1,14 @@
+package com.example.legendofbounca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GyroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gyro);
+    }
+}
