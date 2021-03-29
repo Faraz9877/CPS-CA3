4b61ce7300556e591948d1225da58692e94f4ada
Alireza <azarenejad99@gmail.com>
2021-03-30 04:29:25 +0430

    Complete gravity and moving ball.

--- app/src/main/java/com/example/legendofbounca/Activities/GravityActivity.java
+++ app/src/main/java/com/example/legendofbounca/Activities/GravityActivity.java
@@ -6,17 +6,39 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 import com.example.legendofbounca.Config.Config;
 import com.example.legendofbounca.R;
 
+import java.util.Random;
+
 public class GravityActivity extends AppCompatActivity implements SensorEventListener{
 
-    public float readSensorTimestamp = 1;
-    private View movingBall;
+    //Sensor
     private SensorManager sensorManager;
+    private Sensor gravitySensor;
+
+    public float readSensorTimestamp = 1;
+
+    // Screen layout
+    int layoutRight;
+    int layoutBottom;
+
+    // Ball positions
+    private float x;
+    private float y;
+    private double vx = 0;
+    private double vy = 0;
+    private double fx;
+    private double fy;
+    int movingBallWidth;
+    int movingBallHeight;
+    private View movingBall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +46,28 @@ public class GravityActivity extends AppCompatActivity implements SensorEventLis
         setContentView(R.layout.activity_gravity);
         this.movingBall = findViewById(R.id.movingBall);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+    }
+
+    public void screenClick(View view) {
+        View layout = findViewById(R.id.layout);
+        this.layoutRight = layout.getRight();
+        this.layoutBottom = layout.getBottom();
+
+        this.movingBallWidth = movingBall.getWidth();
+        this.movingBallHeight = movingBall.getHeight();
+
+        Random random = new Random();
+        this.x = random.nextInt(this.layoutRight - 1);
+        this.y = random.nextInt(this.layoutBottom - 1);
+        this.vx = 0;
+        this.vy = 0;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        sensorManager.registerListener(this,
-                sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
@@ -49,15 +86,42 @@ public class GravityActivity extends AppCompatActivity implements SensorEventLis
         if (sensorEvent.sensor.getType() == Sensor.TYPE_GRAVITY) {
             float dT = (sensorEvent.timestamp - readSensorTimestamp) * Config.NS2US;
             if (dT > Config.READ_SENSOR_RATE) {
-                double gravityX = sensorEvent.values[0];
+                double gravityX = - sensorEvent.values[0];
                 double gravityY = sensorEvent.values[1];
                 double gravityZ = sensorEvent.values[2];
-                // TODO: update ball with this three value.
-                System.out.println("gravityX:" + gravityX + " gravityY:" + gravityY + " gravityZ:" + gravityZ);
                 readSensorTimestamp = sensorEvent.timestamp;
+                moveBall(gravityX, gravityY, gravityZ, dT);
             }
         }
     }
+
+    public void moveBall(double gravityX, double gravityY, double gravityZ, float dT) {
+        final double time_slice = dT * Config.US2S;
+        fx = vx == 0 ? (gravityX - (gravityZ * Config.MU_S)) : (gravityX - (gravityZ * Config.MU_K));
+        fy = vy == 0 ? (gravityY - (gravityZ * Config.MU_S)) : (gravityY - (gravityZ * Config.MU_K));
+
+        double ax = fx / Config.MASS;
+        double ay = fy / Config.MASS;
+
+
+        double newX = (0.5) * ax * Math.pow(time_slice, 2) + vx * time_slice + x;
+        double newY = (0.5) * ay * Math.pow(time_slice, 2) + vy * time_slice + y;
+
+        int RIGHTEST_POSITION = this.layoutRight - this.movingBallWidth;
+        int BOTTOMMOST_POSITION = this.layoutBottom - this.movingBallHeight;
+        x = (newX >= RIGHTEST_POSITION) ? RIGHTEST_POSITION : (float) ((newX <= 0) ? 0 : newX);
+        y = (newY >= BOTTOMMOST_POSITION) ? BOTTOMMOST_POSITION : (float) ((newY <= 0) ? 0 : newY);
+
+        double newVX = ax * time_slice + vx;
+        double newVY = ay * time_slice + vy;
+
+        vx = (newX >= RIGHTEST_POSITION || newX <= 0) ? -newVX * Config.LOSS_COEFFICIENT : newVX;
+        vy = (newY >= BOTTOMMOST_POSITION || newY <= 0) ? -newVY * Config.LOSS_COEFFICIENT : newVY;
+
+        movingBall.setX((float) this.x);
+        movingBall.setY((float) this.y);
+    }
+
 
 
 }
--- app/src/main/java/com/example/legendofbounca/Config/Config.java
+++ app/src/main/java/com/example/legendofbounca/Config/Config.java
@@ -1,9 +1,11 @@
 package com.example.legendofbounca.Config;
 
 public class Config {
-    public static final double Mass = 0.05;
+    public static final double MASS = 0.05;
     public static final double MU_S = .15;
     public static final double MU_K = .1;
     public static final int READ_SENSOR_RATE = 20; // sensor read rate in microsecond
     public static final float NS2US = 1.0f / 1000.0f; // ns to microsecond
+    public static final float US2S = 1.0f / 1000000.0f; // ns to microsecond
+    public static final double LOSS_COEFFICIENT = .9;
 }
