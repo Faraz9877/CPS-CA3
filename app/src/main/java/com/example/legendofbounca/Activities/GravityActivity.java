4a810a3be7c5654fd03835b0d10cb4f2c2a105fa
Alireza <azarenejad99@gmail.com>
2021-03-29 23:49:59 +0430

    Refactor reading from sensors.

--- app/src/main/java/com/example/legendofbounca/Activities/GravityActivity.java
+++ app/src/main/java/com/example/legendofbounca/Activities/GravityActivity.java
@@ -12,46 +12,52 @@ import android.view.View;
 import com.example.legendofbounca.Config.Config;
 import com.example.legendofbounca.R;
 
-public class GravityActivity extends AppCompatActivity {
+public class GravityActivity extends AppCompatActivity implements SensorEventListener{
 
     public float readSensorTimestamp = 1;
     private View movingBall;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gravity);
         this.movingBall = findViewById(R.id.movingBall);
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        setupGravitySensor(sensorManager);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onStop() {
+        sensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_GRAVITY) {
+            float dT = (sensorEvent.timestamp - readSensorTimestamp) * Config.NS2US;
+            if (dT > Config.READ_SENSOR_RATE) {
+                double gravityX = sensorEvent.values[0];
+                double gravityY = sensorEvent.values[1];
+                double gravityZ = sensorEvent.values[2];
+                // TODO: update ball with this three value.
+                System.out.println("gravityX:" + gravityX + " gravityY:" + gravityY + " gravityZ:" + gravityZ);
+                readSensorTimestamp = sensorEvent.timestamp;
+            }
+        }
     }
 
 
-    private void setupGravitySensor(SensorManager sensorManager){
-        Sensor gravity_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-
-        SensorEventListener gravitySensorListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                float dT = (sensorEvent.timestamp - readSensorTimestamp) * Config.NS2US;
-                if (dT > Config.READ_SENSOR_RATE) {
-                    double gravityX = sensorEvent.values[0];
-                    double gravityY = sensorEvent.values[1];
-                    double gravityZ = sensorEvent.values[2];
-                    // TODO: update ball with this three value.
-                    System.out.println("gravityX:" + gravityX + " gravityY:" + gravityY + " gravityZ:" + gravityZ);
-                    readSensorTimestamp = sensorEvent.timestamp;
-                }
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
-
-        sensorManager.registerListener(gravitySensorListener, gravity_sensor,
-                SensorManager.SENSOR_DELAY_FASTEST);
-
-  }
 }
--- app/src/main/java/com/example/legendofbounca/Activities/GyroActivity.java
+++ app/src/main/java/com/example/legendofbounca/Activities/GyroActivity.java
@@ -12,51 +12,56 @@ import android.view.View;
 import com.example.legendofbounca.Config.Config;
 import com.example.legendofbounca.R;
 
-public class GyroActivity extends AppCompatActivity {
+public class GyroActivity extends AppCompatActivity implements SensorEventListener {
 
     private float readSensorTimestamp = 1;
 
     private View movingBall;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gyro);
         this.movingBall = findViewById(R.id.movingBall);
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        setupGyroscopeSensor(sensorManager);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
     }
 
-    private void setupGyroscopeSensor(SensorManager sensorManager){
-        Sensor gravity_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_GAME);
+    }
 
-        SensorEventListener gyroscopeSensorListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                float dT = (sensorEvent.timestamp - readSensorTimestamp) * Config.NS2US;
-                if (dT > Config.READ_SENSOR_RATE) {
-                    // rate of rotation around the x axis
-                    double rotationAroundX = sensorEvent.values[0];
-                    // rate of rotation around the y axis
-                    double rotationAroundY = sensorEvent.values[1];
-                    // rate of rotation around the z axis
-                    double rotationAroundZ = sensorEvent.values[2];
-                    // TODO: update ball with this three value.
-                    System.out.println("rotationAroundX:" + rotationAroundX +
-                            " rotationAroundY:" + rotationAroundY +
-                            " rotationAroundZ:" + rotationAroundZ);
-                    readSensorTimestamp = sensorEvent.timestamp;
-                }
+    @Override
+    protected void onStop() {
+        sensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            float dT = (sensorEvent.timestamp - readSensorTimestamp) * Config.NS2US;
+            if (dT > Config.READ_SENSOR_RATE) {
+                // rate of rotation around the x axis
+                double rotationAroundX = sensorEvent.values[0];
+                // rate of rotation around the y axis
+                double rotationAroundY = sensorEvent.values[1];
+                // rate of rotation around the z axis
+                double rotationAroundZ = sensorEvent.values[2];
+                // TODO: update ball with this three value.
+                System.out.println("rotationAroundX:" + rotationAroundX +
+                        " rotationAroundY:" + rotationAroundY +
+                        " rotationAroundZ:" + rotationAroundZ);
+                readSensorTimestamp = sensorEvent.timestamp;
             }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
-
-        sensorManager.registerListener(gyroscopeSensorListener, gravity_sensor,
-                SensorManager.SENSOR_DELAY_FASTEST);
-
+        }
     }
 }
