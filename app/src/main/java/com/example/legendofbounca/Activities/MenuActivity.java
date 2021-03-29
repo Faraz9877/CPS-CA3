3d9a89862c81930da4e08a08491a45bdf809126d
Aryan <a94h.73@gmail.com>
2021-03-29 20:56:18 +0430

    added menu button listeners

--- app/src/main/java/com/example/legendofbounca/Activities/MenuActivity.java
+++ app/src/main/java/com/example/legendofbounca/Activities/MenuActivity.java
@@ -5,26 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.legendofbounca.R;
 
 public class MenuActivity extends AppCompatActivity {
-
-    public void gyroscopeButtonClicked(View view) {
-        Intent intent = new Intent(this, GyroActivity.class);
-        startActivity(intent);
-    }
-
-    public void gravityButtonClicked(View view) {
-        Intent intent = new Intent(this, GravityActivity.class);
-        startActivity(intent);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button gravityButton = findViewById(R.id.gravityButton);
+        Button gyroButton = findViewById(R.id.gyroscopeButton);
 
+        gravityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, GravityActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gyroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, GyroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
