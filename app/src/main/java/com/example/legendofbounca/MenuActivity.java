08b8b75390e5af388e4cde814b0495bac97c97b1
Alireza <azarenejad99@gmail.com>
2021-03-29 20:35:55 +0430

    Completing Menu Activity.

--- app/src/main/java/com/example/legendofbounca/MenuActivity.java
+++ app/src/main/java/com/example/legendofbounca/MenuActivity.java
@@ -2,9 +2,21 @@ package com.example.legendofbounca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
+
+    public void gyroscopeButtonClicked(View view) {
+        Intent intent = new Intent(this, GyroActivity.class);
+        startActivity(intent);
+    }
+
+    public void gravityButtonClicked(View view) {
+        Intent intent = new Intent(this, GravityActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
