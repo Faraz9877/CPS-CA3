04e582793e7b54e1eb1a05980790031a5ec0d466
Alireza <azarenejad99@gmail.com>
2021-03-30 13:08:40 +0430

    change config value.

--- app/src/main/java/com/example/legendofbounca/Config/Config.java
+++ app/src/main/java/com/example/legendofbounca/Config/Config.java
@@ -3,7 +3,7 @@ package com.example.legendofbounca.Config;
 public class Config {
     public static final double MASS = 0.05;
     public static final double MU_S = .15;
-    public static final double MU_K = .1;
+    public static final double MU_K = .07;
     public static final int READ_SENSOR_RATE = 20; // sensor read rate in microsecond
     public static final float NS2US = 1.0f / 1000.0f; // ns to microsecond
     public static final float US2S = 1.0f / 1000000.0f; // ns to microsecond
